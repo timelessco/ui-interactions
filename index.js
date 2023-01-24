f82905196c20f1a38e7a79bf06383e1493c1a784
Karthik-B-06 <karthik06051996@gmail.com>
2023-01-24 12:09:05 +0530

    feat: :building_construction: integrate expo

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {registerRootComponent} from 'expo';
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
