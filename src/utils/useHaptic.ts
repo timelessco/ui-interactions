368bad2c1ecc4791ee396ca4b8ea7b717d56e5c7
Karthik-B-06 <karthik06051996@gmail.com>
2023-02-23 10:41:50 +0530

    feat: :sparkles: update useHaptic hook to include success, warning & error haptic feedbacks

--- src/utils/useHaptic.ts
+++ src/utils/useHaptic.ts
@@ -2,7 +2,14 @@ import { useCallback, useMemo } from "react";
 import { Platform } from "react-native";
 import * as Haptics from "expo-haptics";
 
-type FeedbackType = "light" | "medium" | "heavy" | "selection";
+type FeedbackType =
+  | "light"
+  | "medium"
+  | "heavy"
+  | "selection"
+  | "success"
+  | "warning"
+  | "error";
 
 export const useHaptic = (feedbackType: FeedbackType = "selection") => {
   const createHapticHandler = useCallback(
@@ -13,6 +20,14 @@ export const useHaptic = (feedbackType: FeedbackType = "selection") => {
     },
     [],
   );
+  const createNotificationFeedback = useCallback(
+    (type: Haptics.NotificationFeedbackType) => {
+      return Platform.OS === "web"
+        ? undefined
+        : () => Haptics.notificationAsync(type);
+    },
+    [],
+  );
 
   const hapticHandlers = useMemo(
     () => ({
@@ -20,8 +35,15 @@ export const useHaptic = (feedbackType: FeedbackType = "selection") => {
       medium: createHapticHandler(Haptics.ImpactFeedbackStyle.Medium),
       heavy: createHapticHandler(Haptics.ImpactFeedbackStyle.Heavy),
       selection: Platform.OS === "web" ? undefined : Haptics.selectionAsync,
+      success: createNotificationFeedback(
+        Haptics.NotificationFeedbackType.Success,
+      ),
+      warning: createNotificationFeedback(
+        Haptics.NotificationFeedbackType.Warning,
+      ),
+      error: createNotificationFeedback(Haptics.NotificationFeedbackType.Error),
     }),
-    [createHapticHandler],
+    [createHapticHandler, createNotificationFeedback],
   );
 
   return hapticHandlers[feedbackType];
