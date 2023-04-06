e53bbd776e39d9f397d0c81f8d0a86eb655c678c
Karthik-B-06 <karthik06051996@gmail.com>
2023-04-06 11:05:19 +0530

    feat: :sparkles: wrap scripbox double side allocation slider

--- src/screens/AllocationSlider.tsx
+++ src/screens/AllocationSlider.tsx
@@ -1,15 +1,26 @@
 import { useCallback } from "react";
-import { LayoutChangeEvent, StatusBar, View } from "react-native";
+import {
+  LayoutChangeEvent,
+  StatusBar,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   Extrapolation,
   interpolate,
+  useAnimatedProps,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
 import Svg, { Path } from "react-native-svg";
 import tailwind from "twrnc";
+
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+Animated.addWhitelistedNativeProps({ text: true });
 
 const DoubleSideArrow = () => {
   return (
@@ -29,7 +40,24 @@ export const AllocationSlider = () => {
   const markerPosition = useSharedValue(0);
   const markerDraggingPosition = useSharedValue(0);
   const sliderWidth = useSharedValue(0);
-
+  const equityShare = useDerivedValue(() => {
+    return interpolate(
+      markerDraggingPosition.value,
+      [0, sliderWidth.value],
+      [0, 100],
+      Extrapolation.CLAMP,
+    );
+  });
+  const equityAnimatedTextProps = useAnimatedProps(() => {
+    return {
+      text: `${Math.round(equityShare.value)}%`,
+    };
+  });
+  const debtAnimatedTextProps = useAnimatedProps(() => {
+    return {
+      text: `${Math.round(100 - equityShare.value)}%`,
+    };
+  });
   const handleLayout = useCallback(
     (event: LayoutChangeEvent) => {
       sliderWidth.value = event.nativeEvent.layout.width;
@@ -89,37 +117,108 @@ export const AllocationSlider = () => {
   return (
     <View style={tailwind.style("flex-1 justify-center px-4")}>
       <StatusBar />
-      <Animated.View style={tailwind.style("px-4")}>
-        <Animated.View
-          onLayout={handleLayout}
-          style={tailwind.style(
-            "relative w-full h-11 rounded-xl bg-[#E3E3E3] overflow-hidden",
-          )}
-        >
+      <Animated.View>
+        <Animated.View style={tailwind.style("bg-white rounded-xl pt-[2px]")}>
           <Animated.View
-            style={[
-              tailwind.style("absolute h-11 bg-[#0EB364]"),
-              filledSliderStyle,
-            ]}
-          />
-          <Animated.View
-            style={[
-              tailwind.style("absolute right-0 h-11 bg-[#1F9EFF]"),
-              secondFilledSliderStyle,
-            ]}
-          />
-          <GestureDetector gesture={panGesture}>
+            style={tailwind.style("flex flex-row items-center h-[48px] px-4")}
+          >
             <Animated.View
-              style={[
-                tailwind.style(
-                  "absolute justify-center items-center h-8 w-8 top-[6px] -left-4 rounded-full bg-white shadow-md",
-                ),
-                markerAnimatedStyles,
-              ]}
+              style={tailwind.style("h-3 w-3 rounded-full bg-[#0EB364]")}
+            />
+            <Animated.View
+              style={tailwind.style(
+                "flex flex-row justify-between items-center w-full pr-4",
+              )}
             >
-              <DoubleSideArrow />
+              <Text
+                style={tailwind.style(
+                  "pl-2.5 text-base font-medium text-[#151414]",
+                )}
+              >
+                Equity
+              </Text>
+              <AnimatedTextInput
+                style={tailwind.style(
+                  "text-base font-medium text-[#63605F] tracking-wide",
+                )}
+                editable={false}
+                value={`${String(Math.round(equityShare.value))}%`}
+                // @ts-ignore
+                animatedProps={equityAnimatedTextProps}
+              />
             </Animated.View>
-          </GestureDetector>
+          </Animated.View>
+          <Animated.View
+            style={tailwind.style(
+              "flex flex-row items-center h-[48px] px-4 border-t border-t-[#EBEAEA]",
+            )}
+          >
+            <Animated.View
+              style={tailwind.style("h-3 w-3 rounded-full bg-[#1F9EFF]")}
+            />
+            <Animated.View
+              style={tailwind.style(
+                "flex flex-row justify-between items-center w-full pr-4",
+              )}
+            >
+              <Text
+                style={tailwind.style(
+                  "pl-2.5 text-base font-medium text-[#151414]",
+                )}
+              >
+                Debt
+              </Text>
+              <AnimatedTextInput
+                style={tailwind.style(
+                  "text-base font-medium text-[#63605F] tracking-wide",
+                )}
+                editable={false}
+                value={`${String(Math.round(100 - equityShare.value))}%`}
+                // @ts-ignore
+                animatedProps={debtAnimatedTextProps}
+              />
+            </Animated.View>
+          </Animated.View>
+          <Text
+            style={tailwind.style(
+              "text-sm font-normal text-[#63605F] px-4 pt-2",
+            )}
+          >
+            Adjust the slider to set allocation
+          </Text>
+          <Animated.View style={tailwind.style("px-4")}>
+            <Animated.View
+              onLayout={handleLayout}
+              style={tailwind.style(
+                "relative w-full h-11 rounded-xl bg-[#E3E3E3] overflow-hidden my-4",
+              )}
+            >
+              <Animated.View
+                style={[
+                  tailwind.style("absolute h-11 bg-[#0EB364]"),
+                  filledSliderStyle,
+                ]}
+              />
+              <Animated.View
+                style={[
+                  tailwind.style("absolute right-0 h-11 bg-[#1F9EFF]"),
+                  secondFilledSliderStyle,
+                ]}
+              />
+              <GestureDetector gesture={panGesture}>
+                <Animated.View
+                  style={[
+                    tailwind.style(
+                      "absolute justify-center items-center h-8 w-8 top-[6px] -left-4 rounded-full bg-white shadow-md",
+                    ),
+                    markerAnimatedStyles,
+                  ]}
+                >
+                  <DoubleSideArrow />
+                </Animated.View>
+              </GestureDetector>
+            </Animated.View>
+          </Animated.View>
         </Animated.View>
       </Animated.View>
     </View>
