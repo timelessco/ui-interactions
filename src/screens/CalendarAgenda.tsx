a4716ec14df20978cc9d7f825e2fcb0f01b32180
Karthik-B-06 <karthik06051996@gmail.com>
2023-07-05 19:16:09 +0530

    feat: :sparkles: set statusbar to dark

--- src/screens/CalendarAgenda.tsx
+++ src/screens/CalendarAgenda.tsx
@@ -1,15 +1,14 @@
+import { StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tailwind from "twrnc";
 
 import { CalendarUI } from "../modules/CalendarUI";
-import TimelineProvider from "../modules/CalendarUI/context/TimelineProvider";
 
 export const CalendarAgenda = () => {
   return (
     <SafeAreaView style={tailwind.style("flex-1 bg-white")}>
-      <TimelineProvider>
-        <CalendarUI />
-      </TimelineProvider>
+      <StatusBar barStyle={"dark-content"} />
+      <CalendarUI />
     </SafeAreaView>
   );
 };
